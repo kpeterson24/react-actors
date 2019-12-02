4cf7cd5722d2026a7b2565c11d1617eb58a654a5
Korey Peterson <korey.t.peterson@gmail.com>
2019-12-02 16:20:34 -0600

    first commit with component files created

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class About extends React.Component {
+
+    render() {
+        return (
+        <>
+        <h2>About</h2>
+        <p> Our actors are well experienced and have many films, shows, and voiceover gigs udner their belt. Samurai? Robots? We gottem'! </p>
+        </>
+        )
+    }     
+}
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Contact extends React.Component {
+
+    render() {
+        return (
+        <>
+        <h2>Contact</h2>
+        <p>555-555-1337</p>
+        <p><a href="mailto:korey.t.peterson@gmail.com">emptymoldcasting@gmail.com</a></p>
+        </>
+        )
+    }     
+}
+
+export default Contact;
